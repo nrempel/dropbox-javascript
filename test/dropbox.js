6c1832fcc31758b4f4070fa19a2092a8a35be8e4
Nick Rempel <nbrempel@gmail.com>
2016-03-20 09:46:06 -0700

    Add test

--- test/dropbox.js
+++ test/dropbox.js
@@ -12,3 +12,10 @@ test('Dropbox() should have sane defaults', function (t) {
   t.same(dropbox.api.timeout, 120000);
   t.end();
 });
+
+test('dropbox.setApiKey() should correctly set the key', function (t) {
+  var dropbox = new Dropbox('api-key');
+  dropbox.setApiKey('other-key');
+  t.same(dropbox.api.auth, 'Bearer other-key');
+  t.end();
+});
