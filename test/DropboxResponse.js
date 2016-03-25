35dac7f888ec7a1abbd36ba9ddabf4f706420703
Nick Rempel <nbrempel@gmail.com>
2016-03-25 00:39:36 -0700

    Add tests for DropboxResponse

---
+++ test/DropboxResponse.js
@@ -0,0 +1,19 @@
+const test = require('tape').test;
+const DropboxResponse = require('../lib/DropboxResponse');
+
+test('DropboxResponse should initialize correctly', (t) => {
+  const response = new DropboxResponse('statusCode', 'statusMessage', {});
+  t.same(response.statusCode, 'statusCode');
+  t.same(response.statusMessage, 'statusMessage');
+  t.same(response.data, {});
+  t.end();
+});
+
+test('DropboxResponse should fail if called as function', (t) => {
+  t.throws(() => {
+    /*eslint-disable*/
+    const dropbox = DropboxResponse();
+    /*eslint-enable*/
+  });
+  t.end();
+});
